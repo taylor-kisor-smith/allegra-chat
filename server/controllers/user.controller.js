08438a835816392aebc9f90a8dc841d8aef2f7cb
taylor-kisor-smith <taylor.kisor-smith@codazen.com>
2014-09-26 00:23:51 -0700

    Add user controller

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var mongoose = require('mongoose')
+	Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	username: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	}
+});
+
+var User = mongoose.model('User', userSchema);
+
+var userController = {
+	
+	insert: function(username, password) {
+		var user = new User({'username': username, 'password': password});
+		user.save(function (err) {
+			if (err) return console.error(err);
+		});
+	},
+
+	findByUsername: function(username, callback) {
+		User.find({'username': username.trim()}, callback);
+	}
+};
+
+module.exports = userController;
